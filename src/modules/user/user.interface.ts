96dbde3f80e0580fd2d09d265638eadae20e02da
Md. Sadiq Ibn Mas-Ud <sajidnsubd@gmail.com>
2026-05-09 15:14:42 +0600

    image upload cloudinary

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,4 +1,5 @@
 export interface IUpdateUser {
-  name?: string;
+  name?:  string;
   phone?: string;
+  image?: string; 
 }
--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -3,4 +3,5 @@ import { z } from 'zod';
 export const updateUserSchema = z.object({
   name: z.string().min(1).optional(),
   phone: z.string().min(1).optional(),
+  image: z.string().url().optional().or(z.literal('')),
 });
